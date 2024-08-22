05e23cf13e1bfa9057bfd388fc622d63cb2618f9
Tyler <tylercarrol@hotmail.com>
2024-08-21 21:36:34 -0400

    Create copyright attribute retriever (#4)

--- TJC.AssemblyExtensions.Tests/Attributes/CommonAttributesExtensionsTests.cs
+++ TJC.AssemblyExtensions.Tests/Attributes/CommonAttributesExtensionsTests.cs
@@ -18,5 +18,31 @@ namespace TJC.AssemblyExtensions.Tests.Attributes
             // Assert
             Assert.AreEqual("TJC.AssemblyExtensions.Tests", result);
         }
+
+        [TestMethod]
+        public void AssemblyCopyright_DoNotReplaceSymbol()
+        {
+            // Arrange
+            var assembly = Assembly.GetExecutingAssembly();
+
+            // Act
+            var result = assembly.GetCopyright(replaceCopyrightSymbolWithC: false);
+
+            // Assert
+            Assert.AreEqual("Test Copyright © 2024", result);
+        }
+
+        [TestMethod]
+        public void AssemblyCopyright_ReplaceSymbolWithC()
+        {
+            // Arrange
+            var assembly = Assembly.GetExecutingAssembly();
+
+            // Act
+            var result = assembly.GetCopyright(replaceCopyrightSymbolWithC: true);
+
+            // Assert
+            Assert.AreEqual("Test Copyright (C) 2024", result);
+        }
     }
 }
--- TJC.AssemblyExtensions/Attributes/CommonAttributesExtensions.cs
+++ TJC.AssemblyExtensions/Attributes/CommonAttributesExtensions.cs
@@ -6,4 +6,13 @@ public static class CommonAttributesExtensions
 {
     public static string GetTitle(this Assembly assembly) =>
         assembly.GetAssemblyAttribute<AssemblyTitleAttribute>()?.Title ?? string.Empty;
+
+    public static string GetCopyright(this Assembly assembly, bool replaceCopyrightSymbolWithC = false)
+    {
+        var copyright = assembly.GetAssemblyAttribute<AssemblyCopyrightAttribute>()?.Copyright ?? string.Empty;
+
+        if (replaceCopyrightSymbolWithC)
+            copyright = copyright.Replace("©", "(C)");
+        return copyright;
+    }
 }
