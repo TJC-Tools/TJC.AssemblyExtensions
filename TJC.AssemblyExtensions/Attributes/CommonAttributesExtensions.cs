3cda4e2810d2ca110ac780d3fecba7b9ae077224
Tyler <tylercarrol@hotmail.com>
2024-09-03 21:48:11 -0400

    Create Get License Method for Assembly (#15)

--- TJC.AssemblyExtensions/Attributes/CommonAttributesExtensions.cs
+++ TJC.AssemblyExtensions/Attributes/CommonAttributesExtensions.cs
@@ -18,4 +18,20 @@ public static class CommonAttributesExtensions
             copyright = copyright.Replace("©", "(C)");
         return copyright;
     }
+
+    /// <summary>
+    /// Retrieves the contents of the LICENSE file embedded in the assembly.
+    /// <para>Embed the license file like so: <![CDATA[<EmbeddedResource Include="..\LICENSE"/>]]></para>
+    /// </summary>
+    /// <param name="assembly"></param>
+    /// <returns></returns>
+    public static string GetLicense(this Assembly assembly)
+    {
+        using var stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.LICENSE");
+        if (stream == null)
+            return string.Empty;
+        using var reader = new StreamReader(stream);
+        var content = reader.ReadToEnd();
+        return content;
+    }
 }
