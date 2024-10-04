0f3e51ca26e661c351bbf4c9dca11ac6dc3344a4
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-04 14:14:03 -0400

    ✨ feature: Add Changelog Extensions

---
+++ TJC.AssemblyExtensions/Attributes/ChangelogExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace TJC.AssemblyExtensions.Attributes;
+
+public static partial class CommonAttributeExtensions
+{
+    /// <summary>
+    /// Retrieves the contents of the CHANGELOG.md file embedded in the assembly.
+    /// <para>Embed the license file like so: <![CDATA[<EmbeddedResource Include="..\CHANGELOG.md"/>]]></para>
+    /// </summary>
+    /// <param name="assembly"></param>
+    /// <returns></returns>
+    public static string GetChangelog(this Assembly assembly)
+    {
+        using var stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.CHANGELOG.md");
+        if (stream == null)
+            return string.Empty;
+        using var reader = new StreamReader(stream);
+        var content = reader.ReadToEnd();
+        return content;
+    }
+}
