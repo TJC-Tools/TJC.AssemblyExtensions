c334ff104fde03ed986d12f8724988d8a45b8632
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-04 14:04:34 -0400

    Migrate Extensions

---
+++ TJC.AssemblyExtensions/Attributes/CopyrightExtensions.cs
@@ -0,0 +1,25 @@
+﻿using TJC.StringExtensions.Lines;
+
+namespace TJC.AssemblyExtensions.Attributes;
+
+public static partial class CommonAttributeExtensions
+{
+    public static string GetCopyright(this Assembly assembly, bool replaceCopyrightSymbolWithC = false)
+    {
+        var copyright = assembly.GetAssemblyAttribute<AssemblyCopyrightAttribute>()?.Copyright ?? string.Empty;
+
+        // (Optional) Replace the copyright symbol with (C)
+        if (replaceCopyrightSymbolWithC)
+            copyright = copyright.Replace("©", "(C)");
+
+        // Normalize line endings
+        var result = string.Empty;
+        foreach (var line in copyright.SplitNewLine())
+            result += $"{line.Trim()}{Environment.NewLine}";
+
+        // Trim the trailing newline
+        result = result.Trim();
+
+        return result;
+    }
+}
